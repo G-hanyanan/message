bc14be0900375d819bc9be836399bfaa8de09314
G-hanyanan <1209848443@qq.com>
2020-02-19 18:14:10 +0800

    创建一个users自定义模块，控制users文件

---
+++ utils/users.js
@@ -0,0 +1,41 @@
+// 核心模块
+const fs = require('fs')
+const path = require('path')
+
+// 消除魔术数
+const MSG = "../database/users.json"
+const DATA_FILE = path.join(__dirname, MSG)
+
+// 封装get方法
+const get = () => {
+    // 读users文件
+    let str = fs.readFileSync(DATA_FILE, 'utf8')
+    // 将读出来的数据转为DOM结构
+    let arr = JSON.parse(str)
+    return arr
+}
+
+const add = (userName, pwd, coverUrl) => {
+    let arr = get();
+    // 设置自动增长的ID
+    let id = arr.length ? arr[arr.length - 1][id] + 1 : 1;
+    // 新增的数据
+    let obj = {
+        id,
+        userName,
+        pwd,
+        coverUrl,
+        dt: Date.now()
+    }
+    // 将新增的数据添加到数组中
+    arr.push(obj)
+    // 将数组转为JSON格式，重新写入users文件
+    fs.writeFileSync(DATA_FILE, JSON.stringify(arr))
+
+    return arr
+}
+
+module.exports = {
+    get,
+    add
+}
