35af662bc95206513c35e2cc7efba72dd8868396
G-hanyanan <1209848443@qq.com>
2020-02-20 16:36:44 +0800

    登录成功后通过session设置凭证

--- app/app_server.js
+++ app/app_server.js
@@ -1,25 +1,27 @@
+
 // 第三方模块
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser')
 const multer = require('multer')
+const session = require('express-session')
 
-// express包初始化
-let server = express();
+// 第三方模块初始化
+// 服务器框架
+const server = express()
 // form传参，文件处理
-const upload = multer({ dest: " uploads/" })
+const upload = multer({ dest: " ../uploads/" })
 // post接口将参数转为对象工具
 server.use(bodyParser.urlencoded({ extended: false }))
-
-// 自定义模块
-const user = require('../back-end/utils/users');
+// session配置
+let conf = {
+    secret: '123456', //加密字符串
+    resave: false, // 强制保存
+    saveUninitialized: false // 强制将未初始化的session存储  
+};
+server.use(session(conf))
 
 // 托管静态资源
 server.use(express.static('../public/'))
-
-server.get('/', (req, res) => {
-    res.send('OK')
-
-})
 
 // 注册用户接口
 server.post('/api/addUser', upload.single('cover'), (req, res) => {
@@ -47,6 +49,8 @@ server.get('/api/login', (req, res) => {
             code: 200,
             msg: '登录成功'
         }
+        // 登录成功发钥匙
+        req.session.userName = userName
         res.send(obj)
     } else {
         let obj = {
